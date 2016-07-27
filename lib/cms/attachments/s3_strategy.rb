9887965bc896b7d9862e654cf496d4ac62ca9a42
brandnooz <welz@brandnooz.de>
2016-07-27 12:09:42 +0200

    Update s3_strategy.rb

--- lib/cms/attachments/s3_strategy.rb
+++ lib/cms/attachments/s3_strategy.rb
@@ -10,7 +10,7 @@ module Cms
       #     1. No security, all files are assumed to be public
       #     2. CNAMEs are not supported.
       def self.send_attachment(attachment, controller)
-        controller.redirect_to attachment.url
+        controller.redirect_to attachment.url, :protocol => 'https://', 
         
         # Possible s3_cname implementation
         # if Cms::S3.options[:s3_cname]
